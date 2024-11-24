b556751eb05e1c143971c4f96168f8e195e97040
punithacasi <punithacasi@gmail.com>
2024-11-24 19:14:13 +0100

    Exercise 4.7 atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('6cbb389c1066429cb4271b222ca988ba').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
