6795c744f3332f855a826e5ec2d8ac4da43e7258
punithacasi <punithacasi@gmail.com>
2024-11-25 11:32:14 +0100

    Exercise 4.8 oop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useState, useEffect } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -11,10 +12,21 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
-  }, [currentCity, currentNOE]);
+    let errorText;
+    if (isNaN(currentNOE)) {
+      errorText = "Please enter a valid number to see the events"
+    } else if (currentNOE < 1) {
+      errorText = "Please enter a number greater than zero to see the events"
+    } else {
+      errorText = ""
+    }
+    setErrorAlert(errorText);
+  }, [currentCity, currentNOE, errorAlert]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -27,8 +39,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
+      <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
       <EventList events={events} />
     </div>
   );
