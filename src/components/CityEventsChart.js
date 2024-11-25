c77c0c5369bee8bd5677b4d85613891cc87978f8
punithacasi <punithacasi@gmail.com>
2024-11-25 23:17:17 +0100

    Exercise 4.10 - x axis resolution fix

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -35,8 +35,11 @@ const CityEventsChart = ({ allLocations, events }) => {
                 }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
-                <YAxis type="number" dataKey="count" name="Number of events" />
+                <XAxis
+                    type="category" dataKey="city" name="City"
+                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+                />
+                <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
             </ScatterChart>
